f4d2bbeb8d989968539613ac2fd1f42565107be4
Dinhlam0308 <dinhlam030805@gmail.com>
2026-06-28 12:13:04 +0700

    backend-new

--- src/main/java/com/toystorage/backend/config/security/CorsConfig.java
+++ src/main/java/com/toystorage/backend/config/security/CorsConfig.java
@@ -19,7 +19,10 @@ public class CorsConfig {
                         .allowedOrigins(
                                 "http://localhost:5173",
                                 "http://localhost:3000",
-                                "http://100.124.247.52:5173"
+                                "http://100.125.139.101:5173",
+                                "http://192.168.100.32:5173"
+
+
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
